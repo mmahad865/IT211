25d6d770f82a86c086e686598e3dce67e78924ab
mahad Ali <mmahad865@gmail.com>
2023-06-27 19:10:12 -0700

    about me

---
+++ week1/script.js
@@ -0,0 +1,9 @@
+    let firstName = "Mahad";
+    let lastName = "Ali";
+    let yearsOfStudy = "4 months";
+    let goal = "work in the tech field";
+
+    let result = "My name is " + firstName + " " + lastName + ".<br>";
+    result += "I have been at SCC for " + yearsOfStudy + " and plan to " + goal + " after completing my courses.";
+
+     document.getElementById("result").innerHTML = result;
